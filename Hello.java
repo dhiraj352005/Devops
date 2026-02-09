11f022e39871665acd9e9ac3ae0b74718e22bfc6
Dhiraj Raut <159889522+dhiraj352005@users.noreply.github.com>
2026-02-09 12:05:49 +0530

    Refactor Hello.java to include a test method

--- Hello.java
+++ Hello.java
@@ -1,16 +1,17 @@
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello Jenkins CI");
-        System.out.println("Build triggered automatically!");
-         System.out.println("Checking CI Automation !");
 
+    public static boolean test() {
+        return true;   
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins CI - Assignment 7");
+
+        if(test()) {
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed!");
+            System.exit(1);
+        }
     }
 }
-
-
-
-
-
-
-
-
