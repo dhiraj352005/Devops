2edaec7a690ba00d5766c6feb7deddfa5d961788
Dhiraj Raut <159889522+dhiraj352005@users.noreply.github.com>
2026-02-09 12:50:03 +0530

    Update Hello.java for Docker integration messages

--- Hello.java
+++ Hello.java
@@ -1,16 +1,14 @@
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello Jenkins CI");
-        System.out.println("Build triggered automatically!");
-         System.out.println("Checking CI Automation !");
 
+    public static void main(String[] args) {
+
+        System.out.println("=== Assignment 8 : Docker Integration ===");
+        System.out.println("Application is running inside a Docker container.");
+        System.out.println("Built and pushed using Jenkins CI/CD pipeline.");
+
+        String javaVersion = System.getProperty("java.version");
+        System.out.println("Java Version inside container: " + javaVersion);
+
+        System.out.println("Dockerized application executed successfully!");
     }
 }
-
-
-
-
-
-
-
-
