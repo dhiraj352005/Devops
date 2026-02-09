dea92e56ff364da75e5efab88a13c12987158c8f
Dhiraj Raut <159889522+dhiraj352005@users.noreply.github.com>
2026-02-09 11:47:00 +0530

    Add CI automation message to Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins CI");
         System.out.println("Build triggered automatically!");
+         System.out.println("Bchecking CI Automation !");
 
     }
 }
