5ef73ddcad3676c3c0c5ee7267e28c72b8fdc75e
Dhiraj Raut <159889522+dhiraj352005@users.noreply.github.com>
2026-02-09 11:41:28 +0530

    Add automatic build message to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins CI");
+        System.out.println("Build triggered automatically!");
+
     }
 }
 
